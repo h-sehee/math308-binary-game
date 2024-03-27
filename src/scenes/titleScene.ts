4cddca33bba6d9c3370e08b6d0821debd4da5dca
rrachelr <rachrobins1@gmail.com>
2024-03-27 16:30:31 -0400

    collectables button

--- src/scenes/titleScene.ts
+++ src/scenes/titleScene.ts
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 
 export default class titleScene extends Phaser.Scene {
     private start: Phaser.GameObjects.Rectangle;
+    private collection: Phaser.GameObjects.Rectangle;
 
     constructor() {
         super({ key: "titleScene" });
@@ -13,12 +14,25 @@ export default class titleScene extends Phaser.Scene {
 
     create() {
         this.add.image(640, 360, "bg");
-        this.start = this.add.rectangle(640, 550, 250, 100, 0x0000).setInteractive().on("pointerdown", () => {this.goToLevel1()});
-        this.add.text(568, 530, "START", {color: "white", fontSize: "48px"})
-    }
 
-    private goToLevel1() {
-        console.log("Level 1");
+        this.start = this.add
+            .rectangle(640, 550, 200, 100, 0x0000)
+            .setInteractive();
+        this.start.on("pointerdown", () => {
+            this.scene.stop("titleScene").launch("level1");
+        });
+        this.add.text(568, 530, "Start", { color: "white", fontSize: "48px" });
+
+        this.collection = this.add
+            .rectangle(1000, 550, 400, 100, 0x0000)
+            .setInteractive();
+        this.collection.on("pointerdown", () => {
+            console.log("collection");
+        });
+        this.add.text(830, 530, "Collectables", {
+            color: "white",
+            fontSize: "48px",
+        });
     }
 
     update() {}
