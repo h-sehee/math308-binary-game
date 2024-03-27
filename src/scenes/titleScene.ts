adb64eb382bef3922926ed3afe84ef9d342340ca
rrachelr <rachrobins1@gmail.com>
2024-03-27 16:08:06 -0400

    start button

--- src/scenes/titleScene.ts
+++ src/scenes/titleScene.ts
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 
 export default class titleScene extends Phaser.Scene {
+    private start: Phaser.GameObjects.Rectangle;
 
     constructor() {
         super({ key: "titleScene" });
@@ -12,6 +13,12 @@ export default class titleScene extends Phaser.Scene {
 
     create() {
         this.add.image(640, 360, "bg");
+        this.start = this.add.rectangle(640, 550, 250, 100, 0x0000).setInteractive().on("pointerdown", () => {this.goToLevel1()});
+        this.add.text(568, 530, "START", {color: "white", fontSize: "48px"})
+    }
+
+    private goToLevel1() {
+        console.log("Level 1");
     }
 
     update() {}
