335a7e82570ee8873694ecffd7dc75d8a4f9e67e
Christian R <rullan@udel.edu>
2024-03-28 17:24:52 -0400

    added movement + boundary collision

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -3,33 +3,35 @@ import Phaser from "phaser";
 export default class GameScene extends Phaser.Scene {
     private wizard: Phaser.Physics.Arcade.Sprite;
     private platforms: Phaser.Physics.Arcade.StaticGroup;
-    //private cursor?: Phaser.Types.Input.Keyboard.CursorKeys;
+    private cursor?: Phaser.Types.Input.Keyboard.CursorKeys;
 
     constructor() {
         super({ key: "GameScene" });
     }
 
     create() {
-        //make background gray
+        //make background black
         this.add.image(600, 400, "background").setScale(2);
 
-        //add level
+        //add floor
         this.platforms = this.physics.add.staticGroup();
         const level: Phaser.Physics.Arcade.Image = this.platforms
             .create(400, 400, "platform")
-            .setScale(2, 1);
+            .setScale(2, 1); //stretch floor
 
         this.add.text(165, 280, "Level A", {
             fontSize: "90px",
             color: "red",
         });
-        this.wizard = this.physics.add.sprite(200, 450, "wizard");
-        this.physics.world.setBounds(190, 170, level.width, level.height);
+        this.wizard = this.physics.add.sprite(300, 450, "wizard");
+        this.physics.world.setBounds(
+            65, //div by 6?
+            170, //div by 2 ish?
+            level.displayWidth,
+            level.displayHeight
+        );
 
         this.wizard.setCollideWorldBounds(true);
-
-        /*         this.physics.world.enable(level);
-        this.physics.add.collider(this.wizard, level); */
 
         this.anims.create({
             key: "idle",
@@ -37,10 +39,22 @@ export default class GameScene extends Phaser.Scene {
             frameRate: 1,
             repeat: -1,
         });
+
+        this.cursor = this.input.keyboard?.createCursorKeys();
     }
 
     update() {
-        this.wizard.setVelocityY(370);
-        this.wizard.anims.play("idle");
+        if (this.cursor?.left.isDown) {
+            this.wizard.setVelocityX(-260);
+        } else if (this.cursor?.right.isDown) {
+            this.wizard.setVelocityX(260);
+        } else if (this.cursor?.up.isDown) {
+            this.wizard.setVelocityY(-260);
+        } else if (this.cursor?.down.isDown) {
+            this.wizard.setVelocityY(260);
+        } else {
+            this.wizard.setVelocity(0);
+            this.wizard.anims.play("idle");
+        }
     }
 }
