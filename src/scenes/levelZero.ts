468a282d6d13c5e9e3cbf945fe3f9c40aa74e3d2
Emilie <emiliea@udel.edu>
2024-04-02 13:48:09 -0400

    sprite wip

--- src/scenes/levelZero.ts
+++ src/scenes/levelZero.ts
@@ -1,13 +1,39 @@
 import Phaser from "phaser";
 
 export default class LevelZero extends Phaser.Scene {
+    private player?: Phaser.Physics.Arcade.Sprite
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
+
     constructor() {
         super({ key: "Level0" });
     }
 
-    preload() {}
+    preload() {
+        this.load.image("gal", "assets/Pink_Monster_Walk_6.png");
+        this.load.image("play", "assets/play-button.png");
+    }
 
-    create() {}
+    create() {
+        const backgroundImage = this.add
+            .image(0, 0, "play")
+            .setOrigin(0, 0);
+        backgroundImage.setScale(
+            this.cameras.main.width / backgroundImage.width,
+            this.cameras.main.height / backgroundImage.height
+        );
+        
+        this.player = this.physics.add.sprite(100, 100, "gal");
+        this.player.setCollideWorldBounds(true);
 
-    update() {}
+        /* this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers("gal", {start:0, end:3}),
+            repeat: -1
+        })
+
+        this.cursors = this.input.keyboard?.createCursorKeys() */
+    }
+
+    update() {
+}
 }
