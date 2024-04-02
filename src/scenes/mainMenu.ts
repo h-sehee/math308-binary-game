9476ff053497b7106e6a0823fa015114e17cd48e
Mikey <michael.j.bocelli@gmail.com>
2024-04-02 00:47:07 -0400

    feat: basic career button and exit button functionality

--- src/scenes/mainMenu.ts
+++ src/scenes/mainMenu.ts
@@ -15,8 +15,31 @@ export default class MainMenu extends Phaser.Scene {
             })
             .setOrigin(1, 0);
 
-        this.add.text(this.cameras.main.centerX, 200, "SCHEDULSINE", {
-            color: "#54d6d2",
-        });
+        this.add
+            .text(this.cameras.main.centerX, 200, "SCHEDULSINE", {
+                color: "#54d6d2",
+                fontSize: "100px",
+            })
+            .setOrigin(0.5, 1);
+
+        const careerMode = this.add.image(200, 400, "career");
+        careerMode
+            .setInteractive()
+            .on("pointerdown", () => this.scene.start("FCFSShift"))
+            .on("pointerover", () => careerMode.setScale(1.1))
+            .on("pointerout", () => careerMode.setScale(1));
+
+        const exitButton = this.add.image(
+            this.cameras.main.width - 200,
+            400,
+            "exit"
+        );
+        exitButton
+            .setInteractive()
+            .on("pointerdown", () => {
+                window.close();
+            })
+            .on("pointerover", () => exitButton.setScale(1.1))
+            .on("pointerout", () => exitButton.setScale(1));
     }
 }
