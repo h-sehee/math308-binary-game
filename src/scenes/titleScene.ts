fc4cec6da752e22eb724c3eee2a22533e358401d
jsmtaing <jostai2003@gmail.com>
2024-04-03 09:53:41 -0400

    Image error in titleScene fixed

--- src/scenes/titleScene.ts
+++ src/scenes/titleScene.ts
@@ -7,7 +7,7 @@ export default class TitleScene extends Phaser.Scene {
     }
 
     create() {
-        const logo = this.add.img(this.cameras.main.width / 2, this.cameras.main.height / 2, 'title_logo');
+        const logo = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'title_logo');
 
         this.tweens.add({
             targets: [logo],
