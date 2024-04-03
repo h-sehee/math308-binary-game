045f851ae3868686e092ecd8b71f03e941cf0096
jsmtaing <jostai2003@gmail.com>
2024-04-03 13:01:39 -0400

    npm install, titlescreen edits

--- src/scenes/titleScene.ts
+++ src/scenes/titleScene.ts
@@ -8,10 +8,10 @@ export default class TitleScene extends Phaser.Scene {
 
     create() {
         //Logo Image
-        this.add.image(this.cameras.main.displayWidth * (1/2), this.cameras.main.displayHeight * (1/4), 'title_logo');
+        this.add.image(this.cameras.main.displayWidth * (1/2), this.cameras.main.displayHeight * (2/5), 'title_logo');
 
         //Play Button
-        const play_btn = this.add.image(this.cameras.main.width * (1/2), 510, 'play_btn');
+        const play_btn = this.add.image(this.cameras.main.width * (1/2), 610, 'play_btn');
         play_btn
             .setInteractive()
             //.on('pointerdown', () => this.scene.start("Game_1"))
