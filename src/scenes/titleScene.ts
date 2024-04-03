6c30fdf9e950046b0ccbb559dbb81820eddc1514
jsmtaing <jostai2003@gmail.com>
2024-04-03 10:03:08 -0400

    More logo edits.

--- src/scenes/titleScene.ts
+++ src/scenes/titleScene.ts
@@ -7,16 +7,8 @@ export default class TitleScene extends Phaser.Scene {
     }
 
     create() {
-        const logo = this.add.image(this.cameras.main.displayWidth * (1/2), this.cameras.main.displayHeight * (1/4), 'title_logo');
-        logo.
-
-        //Supposed to fade in, probably will delete though.
-        this.tweens.add({
-            targets: [logo],
-            alpha: 1,
-            duration: 2000,
-            ease: "Linear",
-        });
+        //Logo Image
+        this.add.image(this.cameras.main.displayWidth * (1/2), this.cameras.main.displayHeight * (1/4), 'title_logo');
     }
 
     
