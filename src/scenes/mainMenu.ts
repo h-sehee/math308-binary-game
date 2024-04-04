699b8fecfb15893eb27d1d14a483e56872307aef
Lizzy Kalfas <lizzykal@udel.edu>
2024-04-04 17:09:53 -0400

    titles

--- src/scenes/mainMenu.ts
+++ src/scenes/mainMenu.ts
@@ -12,12 +12,18 @@ export default class mainMenu extends Phaser.Scene {
         const screenHeight: number = Number(height);
 
         this.add
-            .image(screenWidth, screenHeight, "pond")
+            .image(screenWidth / 2, screenHeight / 2, "pond")
             .setDisplaySize(screenWidth, screenHeight);
 
-        this.add.text(150, 150, "CROSS THE POND");
+        this.add.text(250, 350, "CROSS THE POND", {
+            fontSize: "90px",
+        });
         this.button = this.add
-            .text(100, 100, "Click Here to Start", { color: "#0f000" })
+            .text(500, 450, "Click Here to Start", {
+                color: "#0f000",
+                fontSize: "32px",
+                fixedWidth: 400,
+            })
             .setInteractive();
 
         this.button.on("pointerDown", () => {
