d4c947668ead54a82ff9d93cf7961763d74c0615
jsmtaing <jostai2003@gmail.com>
2024-04-04 11:13:28 -0400

    New assets fix.

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -13,7 +13,7 @@ export default class PreloadScene extends Phaser.Scene {
         });
         this.load.image({
             key: "play_bttn",
-            url: "assets/img/play_button.png",
+            url: "assets/img/play_bttn.png",
         });
         this.load.image("kitchen1", "assets/img/cartoon-kitchen.jpg");
         this.load.image("kitchen2", "assets/img/restaurant_oven.jpg");
