ba3c5383699b3b05cd79567a22298f2291a03bb1
joanna <joancris@udel.edu>
2024-04-04 00:00:29 -0400

    Add additional images

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -14,7 +14,11 @@ export default class PreloadScene extends Phaser.Scene {
                 scale: 1.5
             }
         })
-        this.load.spritesheet("dude", "assets/img/dude.png",
+        this.load.image("kitchen1", "assets/img/cartoon-kitchen.jpg");
+        this.load.image("kitchen2", "assets/img/restaurant_oven.jpg");
+        this.load.image("tomato", "assets/img/Tomato.jpg");
+        this.load.image("inside", "assets/img/Indoor_Restaurant.jpg");
+        this.load.spritesheet("dude", "assets/img/Sprite.png",
         {
             frameWidth: 32,
             frameHeight: 48,
