0c5bf8ba55e90f31b03a0c3dcf7a62c8d641014a
Victor Vasquez <victorbrillos1@gmail.com>
2024-04-04 22:56:43 -0400

    created some of the class for a character

---
+++ src/objects/characterSkeleton.ts
@@ -0,0 +1,32 @@
+abstract class GameCharacter {
+  name: string;
+  health: number;
+  position: Phaser.Math.Vector2;
+  alive: boolean;
+  cost: number;
+
+  constructor(name: string, health: number, position: Phaser.Math.Vector2, cost: number) {
+    this.name = name;
+    this.health = health;
+    this.position = position;
+    this.alive = true;
+    this.cost = cost;
+  }
+
+  abstract attack(): void;
+
+  // Method for the character to take damage. Reduces health and checks for death.
+  takeDamage(damage: number): void {
+    this.health -= damage;
+    if (this.health <= 0) {
+      this.health = 0;
+      this.alive = false;
+      this.remove();
+    }
+  }
+
+  // Method to remove the character from the game, e.g., when health is 0.
+  remove(): void {
+    console.log(`${this.name} has been removed from the game.`);
+  }
+}
