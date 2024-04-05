e3f4031d989aed19bcda80d8ff90ebbbf44e26f2
SEHEE <hsehee@udel.edu>
2024-04-04 20:52:34 -0400

    Added HP bar on the enemy

--- src/enemies/redEyesSkeleton.ts
+++ src/enemies/redEyesSkeleton.ts
@@ -11,8 +11,9 @@ export default class RedEyesSkeleton extends Phaser.Physics.Arcade.Sprite {
 
     private healthState = HealthState.IDLE;
     private damageTime = 0;
-
     private _health = 20;
+    private maxHealth = 20;
+    private healthBar: Phaser.GameObjects.Graphics;
 
     get health() {
         return this._health;
@@ -27,6 +28,7 @@ export default class RedEyesSkeleton extends Phaser.Physics.Arcade.Sprite {
     ) {
         super(scene, x, y, texture, frame);
         this.anims.play("redEyesSkeleton-idle-down");
+        this.createHealthBar();
     }
 
     setTarget(target: Phaser.Physics.Arcade.Sprite) {
@@ -46,12 +48,43 @@ export default class RedEyesSkeleton extends Phaser.Physics.Arcade.Sprite {
         if (this._health <= 0) {
             this.healthState = HealthState.DEAD;
             this.setVelocity(0, 0);
-            this.destroy();
+            this.scene.tweens.add({
+                targets: this,
+                alpha: 0,
+                duration: 300,
+                onComplete: () => {
+                    this.healthBar.destroy();
+                    this.destroy();
+                },
+            });
         } else {
             this.setTint(0xff0000);
             this.healthState = HealthState.DAMAGE;
             this.damageTime = 0;
         }
+    }
+
+    createHealthBar() {
+        this.healthBar = this.scene.add.graphics();
+        this.updateHealthBarPosition();
+        this.updateHealthBarSize();
+    }
+
+    updateHealthBarPosition() {
+        this.healthBar.x = this.x - 15;
+        this.healthBar.y = this.y - 20;
+    }
+
+    updateHealthBarSize() {
+        this.healthBar.clear();
+
+        this.healthBar.fillStyle(0x000000, 0.8);
+        this.healthBar.fillRect(0, 0, 25, 3);
+
+        const width = (this.health / this.maxHealth) * 25;
+
+        this.healthBar.fillStyle(0xff0000, 1);
+        this.healthBar.fillRect(0, 0, width, 3);
     }
 
     preUpdate(t: number, dt: number) {
@@ -92,6 +125,11 @@ export default class RedEyesSkeleton extends Phaser.Physics.Arcade.Sprite {
             this.anims.play("redEyesSkeleton-run-up", true);
             this.body.offset.y = 4;
         }
-        this.scene.physics.moveTo(this, this.target.x, this.target.y, 50);
+        //this.scene.physics.moveTo(this, this.target.x, this.target.y, 50);
+    }
+
+    update() {
+        this.updateHealthBarPosition();
+        this.updateHealthBarSize();
     }
 }
