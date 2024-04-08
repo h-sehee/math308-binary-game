f9b15bc31608504ec82f5ef2fd576e9b7998cc23
rachel-sison <RSISON@UDEL.EDU>
2024-04-08 12:33:56 -0400

    added stones

--- src/scenes/levelOne.ts
+++ src/scenes/levelOne.ts
@@ -1,6 +1,7 @@
-import Phaser from "phaser";
+import Phaser, { GameObjects } from "phaser";
 
 export default class levelOne extends Phaser.Scene {
+    private stone?: Phaser.Physics.Arcade.StaticGroup;
     constructor() {
         super({ key: "levelOne" });
     }
@@ -16,7 +17,19 @@ export default class levelOne extends Phaser.Scene {
             .image(screenWidth / 2, screenHeight / 2, "pond")
             .setDisplaySize(screenWidth, screenHeight);
 
-        this.add.image(300, 400, "duck");
+        this.add.image(150, 500, "duck").setScale(0.4);
+
+        this.stone = this.physics.add.staticGroup();
+
+        const stone1 = this.stone?.create(500, 400, "stone");
+        const stone2 = this.stone?.create(275, 500, "stone");
+        const stone3 = this.stone?.create(700, 600, "stone");
+        const stone4 = this.stone?.create(750, 450, "stone");
+
+        stone1.setScale(0.5, 0.4);
+        stone2.setScale(0.5, 0.4);
+        stone3.setScale(0.5, 0.4);
+        stone4.setScale(0.5, 0.4);
     }
 
     update() {}
