d5ec5caf841f239eccde69068016c000305b7242
rrachelr <rachrobins1@gmail.com>
2024-04-10 14:36:11 -0400

    changed level 1 background

--- src/scenes/level1.ts
+++ src/scenes/level1.ts
@@ -8,13 +8,13 @@ export default class level1 extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("background", "assets/img/background.jpg");
+        this.load.image("background", "assets/img/background.png");
     }
 
     create() {
         this.add.image(350, 360, "background");
         this.add.rectangle(640, 0, 1280, 150, 0x0000);
-        this.levelText = this.add.text(545,10, "Level 1", {
+        this.levelText = this.add.text(545, 10, "Level 1", {
             fontSize: "48px",
         });
         this.add.rectangle(1000, 250, 600, 350, 0xffff);
