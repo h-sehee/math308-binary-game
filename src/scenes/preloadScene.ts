825b611461ca9183e9c0a588849671eb50175cb8
SEHEE <hsehee@udel.edu>
2024-04-10 20:40:40 -0400

    Changed screen size

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -10,7 +10,7 @@ export default class PreloadScene extends Phaser.Scene {
         //this.load.image("maze", "assets/img/maze.jpeg");
         //this.load.image("Minotaur", "assets/img/Minotaur.jpg");
         this.load.image("base_tiles", "assets/tileset.png");
-        this.load.tilemapTiledJSON("tilemap", "assets/dungeon_background.json");
+        this.load.tilemapTiledJSON("tilemap", "assets/maze_background.json");
 
         this.load.atlas("faune", "assets/fauna.png", "assets/fauna.json");
         this.load.image("sword", "assets/sword_normal.png");
