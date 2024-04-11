74d32b72ae942e194b12606898e9d19c553e2d0e
Jon O'Connell <jjoc0205@gmail.com>
2024-04-11 19:37:27 -0400

    simple drag and drop works

--- src/scenes/level1.ts
+++ src/scenes/level1.ts
@@ -4,7 +4,7 @@ export default class level1 extends Phaser.Scene {
     private levelText: Phaser.GameObjects.Text;
 
     constructor() {
-        super({ key: "level1" });
+        super({ key: "level1" }); 
     }
 
     preload() {
@@ -18,6 +18,7 @@ export default class level1 extends Phaser.Scene {
     create() {
         this.add.image(350, 360, "background");
         this.add.rectangle(640, 0, 1280, 150, 0x0000);
+        //const dropZoneColorBrown = this.add.dropZoneColorBrown(640, 0, 1280, 150).setRectangleDropZone(1280, 150);
         this.levelText = this.add.text(545, 10, "Level 1", {
             fontSize: "48px",
         });
@@ -25,6 +26,7 @@ export default class level1 extends Phaser.Scene {
         this.add.rectangle(1000, 650, 600, 450, 0x9999);
 
         this.add.image(350, 325, "monkey-brown-pirate");
+        const hello = this.input.setDraggable(this.add.text(720,550, "HELLO", {fontSize: "42px"}).setInteractive(), true);
 
         this.add.text(720, 100, "class Monkey:", {
             fontSize: "42px",
@@ -39,6 +41,72 @@ export default class level1 extends Phaser.Scene {
             fontSize: "32px",
             color: "black",
         });
+    //  A drop dropZoneColorBrown
+        const dropZoneColorBrown: Phaser.GameObjects.Zone = this.add.zone(950, 196, 150, 50).setRectangleDropZone(150, 50).setInteractive();
+
+        //  Just a visual display of the dropZoneColorBrown
+        const graphics = this.add.graphics();
+        graphics.lineStyle(2, 0xffff00);
+        if (dropZoneColorBrown.input) {
+            graphics.strokeRect(dropZoneColorBrown.x - dropZoneColorBrown.input.hitArea.width / 2, dropZoneColorBrown.y - dropZoneColorBrown.input.hitArea.height / 2, dropZoneColorBrown.input.hitArea.width, dropZoneColorBrown.input.hitArea.height);
+        }
+
+        this.input.on('drag', (pointer: any, gameObject: { x: any; y: any; }, dragX: any, dragY: any) =>
+        {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+
+        });
+
+        this.input.on('dragenter', (pointer: any, gameObject: Phaser.GameObjects.Text, dropZone: Phaser.GameObjects.Zone) =>
+        {
+            graphics.clear();
+            graphics.lineStyle(2, 0x00ffff);
+            if (dropZoneColorBrown.input) {
+                graphics.strokeRect(dropZone.x - dropZoneColorBrown.input.hitArea.width / 2, dropZoneColorBrown.y - dropZoneColorBrown.input.hitArea.height / 2, dropZoneColorBrown.input.hitArea.width, dropZoneColorBrown.input.hitArea.height);
+            }
+        });
+
+        this.input.on('dragleave', (pointer: any, gameObject: Phaser.GameObjects.Text, dropZone: Phaser.GameObjects.Zone) =>
+        {
+
+            graphics.clear();
+            graphics.lineStyle(2, 0xffff00);
+            if (dropZoneColorBrown.input) {
+                graphics.strokeRect(dropZoneColorBrown.x - dropZoneColorBrown.input.hitArea.width / 2, dropZoneColorBrown.y - dropZoneColorBrown.input.hitArea.height / 2, dropZoneColorBrown.input.hitArea.width, dropZoneColorBrown.input.hitArea.height);
+            }
+        });
+
+        this.input.on('drop', (pointer: any, gameObject: Phaser.GameObjects.Text, dropZone: Phaser.GameObjects.Zone) =>
+        {
+
+            gameObject.x = dropZone.x-50;
+            gameObject.y = dropZone.y-25;
+
+            if (gameObject.input) {
+                gameObject.input.enabled = false;
+            }
+
+        });
+
+        this.input.on('dragend', (pointer: any, gameObject: Phaser.GameObjects.Text, dropped: any) =>
+        {
+            if (!dropped)
+            {
+                if (gameObject.input) {
+                    gameObject.x = gameObject.input.dragStartX;
+                    gameObject.y = gameObject.input.dragStartY;
+                }
+            }
+
+            graphics.clear();
+            graphics.lineStyle(2, 0xffff00);
+            if (dropZoneColorBrown.input) {
+                graphics.strokeRect(dropZoneColorBrown.x - dropZoneColorBrown.input.hitArea.width / 2, dropZoneColorBrown.y - dropZoneColorBrown.input.hitArea.height / 2, dropZoneColorBrown.input.hitArea.width, dropZoneColorBrown.input.hitArea.height);
+            }
+        });
+
     }
 
     update() {}
