578cc604b46153c1c4ca5ebcc15811bc0f5f20ca
Sibyl Roosen <sibyl@udel.edu>
2024-04-11 17:22:23 -0400

    made bullets visable

--- src/scenes/levelOne.ts
+++ src/scenes/levelOne.ts
@@ -3,7 +3,8 @@ import Phaser from "phaser";
 //import FpsText from "../objects/fpsText";
 class Bullet extends Phaser.Physics.Arcade.Sprite {
     constructor(scene: Phaser.Scene, x: number, y: number) {
-        super(scene, x, y, "star.png");
+        super(scene, x, y, "star");
+        scene.add.existing(this);
     }
 
     fire(x: number, y: number, vel: number) {
