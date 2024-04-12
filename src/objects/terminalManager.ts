2c2dc253de4a722d2bdad872adcbeb75f3c4171b
Christian R <rullan@udel.edu>
2024-04-11 21:05:36 -0400

    fixed bug causing multiple calls to GameScene

--- src/objects/terminalManager.ts
+++ src/objects/terminalManager.ts
@@ -6,21 +6,34 @@ export class TerminalManager {
 
     private eventEmitter: Phaser.Events.EventEmitter;
 
-
-    constructor(eventEmitter: Phaser.Events.EventEmitter) {
+    constructor(eventEmitter: Phaser.Events.EventEmitter, fighting: boolean) {
         this.eventEmitter = eventEmitter;
         this.inputElement = document.createElement("input");
+        console.log("why");
         this.inputElement.type = "text";
 
         //styling
-        this.inputElement.style.position = "fixed";
-        this.inputElement.style.bottom = "20px";
-        this.inputElement.style.left = "25%";
-        this.inputElement.style.width = "50%";
-        this.inputElement.style.height = "80px";
-        this.inputElement.style.border = "1px solid #ccc";
-        this.inputElement.style.backgroundColor = "#2D2E2C";
-        this.inputElement.style.color = "white";
+        if (!fighting) {
+            this.inputElement.style.position = "fixed";
+            this.inputElement.style.bottom = "20px";
+            this.inputElement.style.left = "25%";
+            this.inputElement.style.width = "50%";
+            this.inputElement.style.height = "80px";
+            this.inputElement.style.border = "1px solid #ccc";
+            this.inputElement.style.backgroundColor = "#2D2E2C";
+            this.inputElement.style.color = "white";
+        }
+        if (fighting) {
+            this.inputElement.style.position = "fixed";
+            this.inputElement.style.bottom = "20px";
+            this.inputElement.style.left = "25%";
+            this.inputElement.style.width = "50%";
+            this.inputElement.style.height = "160px";
+            this.inputElement.style.border = "1px solid #ccc";
+            this.inputElement.style.backgroundColor = "#2D2E2C";
+            this.inputElement.style.color = "red";
+            this.inputElement.style.paddingBottom = "90px";
+        }
 
         document.body.appendChild(this.inputElement);
 
@@ -32,13 +45,16 @@ export class TerminalManager {
         );
     }
 
-    private handleEnter(event: KeyboardEvent) {
+    public handleEnter(event: KeyboardEvent) {
         if (event.key === "Enter") {
             const userInput = this.inputElement.value;
             console.log("User input:", userInput);
             this.eventEmitter.emit("userInput", userInput);
             // Clear input field after processing
             this.inputElement.value = this.prompt;
+        }
+        if (event.key === " ") {
+            this.inputElement.value += " ";
         }
     }
 }
