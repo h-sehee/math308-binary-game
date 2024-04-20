a0289b77fa7be6239e05c1c13c66490d8e898826
SEHEE <hsehee@udel.edu>
2024-04-20 18:22:26 -0400

    Added favicon

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -35,7 +35,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            gameName: "My Phaser Game",
+            gameName: "MATH308 Binary",
             template: "assets/index.html",
         }),
         new CopyWebpackPlugin({
